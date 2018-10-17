93535a4afbd9081d5d1ccf80e74f23f30c82cf49
1414006272 <1414006272@qq.com>
2018-10-17 18:28:57 +0800

    启动页延时

--- app/src/main/java/com/yxzc/tzl/ui/launch/LaunchActivity.java
+++ app/src/main/java/com/yxzc/tzl/ui/launch/LaunchActivity.java
@@ -3,15 +3,12 @@ package com.yxzc.tzl.ui.launch;
 import android.Manifest;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.FragmentActivity;
 
 import com.tbruyelle.rxpermissions2.Permission;
 import com.yxzc.tzl.base.BaseActivity;
 import com.yxzc.tzl.constants.CommonCode;
 import com.yxzc.tzl.ui.main.Main;
 import com.yxzc.tzl.utils.permission.PermissionUtils;
-
-import java.lang.ref.WeakReference;
 
 /**
  * @Author: HouShengLi
@@ -21,13 +18,10 @@ import java.lang.ref.WeakReference;
  */
 public class LaunchActivity extends BaseActivity {
 
-    private LaunchHandler mHandler = new LaunchHandler(this);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // TODO: 2018/9/27 可以做一些初始化工作
-//        mHandler.sendEmptyMessageDelayed(CommonCode.LAUNCH_MAIN, CommonCode.LAUNCH_DELAY);
         requestPermission();
     }
 
@@ -61,35 +55,12 @@ public class LaunchActivity extends BaseActivity {
     private PermissionUtils mPermissionUtils = new PermissionUtils() {
         @Override
         protected void onPermissionResult(boolean isSuccess, Permission permission, int requestCode) {
-            startMain();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startMain();
+                }
+            }, 1000);
         }
     };
-
-    private static class LaunchHandler extends Handler {
-
-        //弱引用<引用外部类>
-        WeakReference<FragmentActivity> mActivity;
-
-        LaunchHandler(FragmentActivity activity) {
-            //构造创建弱引用
-            mActivity = new WeakReference<FragmentActivity>(activity);
-        }
-
-        @Override
-        public void handleMessage(android.os.Message msg) {
-            //通过弱引用获取外部类.
-            FragmentActivity activity = mActivity.get();
-            //进行非空再操作
-            if (activity != null) {
-                switch (msg.what) {
-                    case CommonCode.LAUNCH_MAIN:
-                        // TODO: 2018/10/10
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    }
-
 }
